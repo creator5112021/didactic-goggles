0a7014de7d266aded0a3c3bcad1af6dfdf1c29cc
Shivansh_Kumar <91822395+creator5112021@users.noreply.github.com>
2023-04-04 14:58:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload() {
+
+}
+
+function setup() {
+  canvas = createCanvas(400, 400)
+  canvas.center()
+  background("white")
+}
+function clearcanvas() {
+    background("white")
+}
